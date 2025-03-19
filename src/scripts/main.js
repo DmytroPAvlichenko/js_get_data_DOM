b58d2fb7c2e450598d82ed4d2a54d0168af01dea
dimon <80713na@gmail.com>
2025-03-20 00:38:34 +0200

    populapion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+
+const numbers = [...population]
+  .map((span) => Number(span.textContent.replace(/\D/g, '')))
+  .filter((num) => !isNaN(num) && num !== 0);
+
+if (numbers.length > 0) {
+  const average = numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+  const rezult = Math.round(average).toLocaleString();
+
+  const averageSpan = document.querySelector('.average-population');
+
+  if (averageSpan) {
+    averageSpan.textContent = rezult;
+  }
+
+  const totalSpan = document.querySelector('.total-population');
+
+  if (totalSpan) {
+    totalSpan.textContent = rezult;
+  }
+}
