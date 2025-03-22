0c1f5600ba977afcf35229518a7e0bdf42644f2c
dimon <80713na@gmail.com>
2025-03-23 00:53:16 +0200

    averege

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const population = document.querySelectorAll('.population');
+const populat = [...population];
 
-const numbers = [...population]
-  .map((span) => Number(span.textContent.replace(/\D/g, '')))
-  .filter((num) => !isNaN(num) && num !== 0);
+const numb = populat.map((span) => Number(span.textContent.replace(/\D/g, '')));
+const totalNumb = numb.filter((num) => !isNaN(num) && num !== 0);
 
-if (numbers.length > 0) {
-  const average = numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
-  const rezult = Math.round(average).toLocaleString();
+if (totalNumb.length > 0) {
+  const average = totalNumb.reduce((sum, num) => sum + num, 0);
+
+  const rezult = Math.round(average / totalNumb.length).toLocaleString();
 
   const averageSpan = document.querySelector('.average-population');
 
@@ -19,6 +20,6 @@ if (numbers.length > 0) {
   const totalSpan = document.querySelector('.total-population');
 
   if (totalSpan) {
-    totalSpan.textContent = rezult;
+    totalSpan.textContent = average.toLocaleString();
   }
 }
